ad1a6ace9f63bc379ef28c863f6cace0b4dbeaa7
Magdy Roshdy <magdy.roshdy@gmail.com>
2014-02-17 18:10:41 +0400

    add paging to Students and Student Guardians listing pages

--- ManarAlSabeel.Web/Controllers/StudentGuardianController.cs
+++ ManarAlSabeel.Web/Controllers/StudentGuardianController.cs
@@ -1,6 +1,7 @@
 ﻿using ManarAlSabeel.Domain.Abstract;
 using ManarAlSabeel.Domain.Entities;
 using ManarAlSabeel.Web.Infrastructure;
+using ManarAlSabeel.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +13,30 @@ namespace ManarAlSabeel.Web.Controllers
 	[ForbiddenRedirectAuthorizeAttribute]
     public class StudentGuardianController : Controller
     {
+		private int PageSize = 25;
 		private ICenterRepository dbRepository;
 		public StudentGuardianController(ICenterRepository repo)
 		{
 			dbRepository = repo;
 		}
 
-        public ActionResult List()
+        public ActionResult List(int page = 1)
         {
-			return View(dbRepository.GetAllStudentGuardians());
+			PagingInfo pagingInfo = new PagingInfo
+			{
+				CurrentPage = page,
+				ItemsPerPage = PageSize,
+				TotalItems = dbRepository.GetAllStudents().Count()
+			};
+
+			IQueryable<StudentGuardian> studentGuardians = dbRepository.GetAllStudentGuardians().Skip((page - 1) * PageSize)
+				.Take(PageSize);
+
+			StudentGuardiansListViewModel model = new StudentGuardiansListViewModel
+			{
+				Guardians = studentGuardians, PagingInfo = pagingInfo
+			};
+			return View(model);
         }
 
 		public ViewResult Edit(int id)
